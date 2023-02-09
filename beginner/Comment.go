31405ccd1436d50a2745e8508fb05ed4974ffc27
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-02-09 07:29:10 +0700

    comment on go

---
+++ beginner/Comment.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// single line comments
+	fmt.Println("The above comment single line")
+
+	/* 
+	multiple line comments
+	*/
+	fmt.Println("The above comment multi-line")
+}
